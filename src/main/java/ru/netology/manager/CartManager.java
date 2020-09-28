1d2fc3f03a81db76651040ea3de8661bbe16b620
Vadim <vadim.kurn@gmail.com>
2020-09-28 13:55:25 +0300

    Logic and tests edited

--- src/main/java/ru/netology/manager/CartManager.java
+++ src/main/java/ru/netology/manager/CartManager.java
@@ -7,26 +7,29 @@ public class CartManager {
   private PurchaseItem[] items = new PurchaseItem[0];
 
   public void add(PurchaseItem item) {
-    // создаём новый массив размером на единицу больше
     int length = items.length + 1;
     PurchaseItem[] tmp = new PurchaseItem[length];
-    // itar + tab
-    // копируем поэлементно
-    // for (int i = 0; i < items.length; i++) {
-    //   tmp[i] = items[i];
-    // }
     System.arraycopy(items, 0, tmp, 0, items.length);
-    // кладём последним наш элемент
     int lastIndex = tmp.length - 1;
     tmp[lastIndex] = item;
     items = tmp;
   }
 
-  public PurchaseItem[] getLast(int returnCount){
+  public PurchaseItem[] getLast() {
     PurchaseItem[] result = new PurchaseItem[items.length];
-    for (int i = 0; i < returnCount & i < result.length; i++) {
-      int index = items.length - i - 1;
-      result[i] = items[index];
+    if (items.length > returnCount) {
+      PurchaseItem[] result1 = new PurchaseItem[returnCount];
+      for (int i = 0; i < returnCount; i++) {
+        int index = items.length - i - 1;
+        result1[i] = items[index];
+      }
+      return result1;
+    }
+    else {
+      for (int i = 0; i < result.length; i++) {
+        int index = items.length - i - 1;
+        result[i] = items[index];
+      }
     }
     return result;
   }
---
+++ src/test/java/ru/netology/manager/CartManagerTest.java
@@ -0,0 +1,59 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.PurchaseItem;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class CartManagerTest {
+    private CartManager manager = new CartManager();
+
+    @Test
+    public  void shouldSetReturnCount(){
+        CartManager manager1 = new CartManager(5);
+        PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
+        PurchaseItem second = new PurchaseItem(2, 2, "second", 1, 1);
+        PurchaseItem third = new PurchaseItem(3, 3, "third", 1, 1);
+        PurchaseItem fourth = new PurchaseItem(4, 4, "fourth", 1, 1);
+        PurchaseItem fifth = new PurchaseItem(5, 5, "fifth", 1, 1);
+        PurchaseItem sixth = new PurchaseItem(6, 6, "sixth", 1, 1);
+        PurchaseItem seventh = new PurchaseItem(7, 7, "seventh", 1, 1);
+        PurchaseItem eighth = new PurchaseItem(8, 8, "eighth", 1, 1);
+        PurchaseItem ninth = new PurchaseItem(9, 9, "ninth", 1, 1);
+        PurchaseItem tenth = new PurchaseItem(10, 10, "tenth", 1, 1);
+        manager1.add(first);
+        manager1.add(second);
+        manager1.add(third);
+        manager1.add(fourth);
+        manager1.add(fifth);
+        manager1.add(sixth);
+        manager1.add(seventh);
+        manager1.add(eighth);
+        manager1.add(ninth);
+        manager1.add(tenth);
+        PurchaseItem[] expected = new PurchaseItem[] {tenth, ninth, eighth, seventh, sixth};
+        PurchaseItem[] actual = manager1.getLast();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void addToCart(){
+        PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
+        manager.add(first);
+        PurchaseItem[] expected = new PurchaseItem[]{first};
+        PurchaseItem[] actual = manager.getLast();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public  void getLast(){
+        PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
+        PurchaseItem second = new PurchaseItem(2, 2, "second", 1, 1);
+        PurchaseItem third = new PurchaseItem(3, 3, "third", 1, 1);
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+        PurchaseItem[] expected = new PurchaseItem[] {third, second, first};
+        PurchaseItem[] actual = manager.getLast();
+        assertArrayEquals(expected, actual);
+    }
+}
