a11c284e2dc637a5390eff8d12c07a9d8e34c2c1
Vadim <vadim.kurn@gmail.com>
2020-10-13 21:29:30 +0300

    CartManagerTest.java added

--- src/test/java/ru/netology/manager/CartManagerTest.java
+++ src/test/java/ru/netology/manager/CartManagerTest.java
@@ -1,59 +1,55 @@
 package ru.netology.manager;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.netology.domain.PurchaseItem;
+import ru.netology.repository.Repository;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.mockito.Mockito.*;
 
+
+@ExtendWith(MockitoExtension.class)
 public class CartManagerTest {
-    /*private CartManager manager = new CartManager();*/
 
-    /*@Test
-    public  void shouldSetReturnCount(){
-        CartManager manager1 = new CartManager(5);
-        PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
-        PurchaseItem second = new PurchaseItem(2, 2, "second", 1, 1);
-        PurchaseItem third = new PurchaseItem(3, 3, "third", 1, 1);
-        PurchaseItem fourth = new PurchaseItem(4, 4, "fourth", 1, 1);
-        PurchaseItem fifth = new PurchaseItem(5, 5, "fifth", 1, 1);
-        PurchaseItem sixth = new PurchaseItem(6, 6, "sixth", 1, 1);
-        PurchaseItem seventh = new PurchaseItem(7, 7, "seventh", 1, 1);
-        PurchaseItem eighth = new PurchaseItem(8, 8, "eighth", 1, 1);
-        PurchaseItem ninth = new PurchaseItem(9, 9, "ninth", 1, 1);
-        PurchaseItem tenth = new PurchaseItem(10, 10, "tenth", 1, 1);
-        manager1.add(first);
-        manager1.add(second);
-        manager1.add(third);
-        manager1.add(fourth);
-        manager1.add(fifth);
-        manager1.add(sixth);
-        manager1.add(seventh);
-        manager1.add(eighth);
-        manager1.add(ninth);
-        manager1.add(tenth);
-        PurchaseItem[] expected = new PurchaseItem[] {tenth, ninth, eighth, seventh, sixth};
-        PurchaseItem[] actual = manager1.getLast();
+    @Mock
+    private Repository repository;
+    @InjectMocks
+    private CartManager manager;
+    private PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
+    private PurchaseItem second = new PurchaseItem(2, 2, "second", 1, 1);
+    private PurchaseItem third = new PurchaseItem(3, 3, "third", 1, 1);
+
+    @Test
+    public void shouldRemoveIfExists() {
+        int idToRemove = 1;
+        PurchaseItem[] returned = new PurchaseItem[]{second, third};
+        doReturn(returned).when(repository).findAll();
+        doNothing().when(repository).removeById(idToRemove);
+
+        manager.removeById(idToRemove);
+        PurchaseItem[] expected = new PurchaseItem[]{third, second};
+        PurchaseItem[] actual = manager.getAll();
         assertArrayEquals(expected, actual);
+
+        verify(repository).removeById(idToRemove);
     }
 
     @Test
-    public void addToCart(){
-        PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
-        manager.add(first);
-        PurchaseItem[] expected = new PurchaseItem[]{first};
-        PurchaseItem[] actual = manager.getLast();
-        assertArrayEquals(expected, actual);
-    }
+    public void shouldNotRemoveIfNotExists() {
+        int idToRemove = 4;
+        PurchaseItem[] returned = new PurchaseItem[]{first, second, third};
+        doReturn(returned).when(repository).findAll();
+        doNothing().when(repository).removeById(idToRemove);
 
-    @Test
-    public  void getLast(){
-        PurchaseItem first = new PurchaseItem(1, 1, "first", 1, 1);
-        PurchaseItem second = new PurchaseItem(2, 2, "second", 1, 1);
-        PurchaseItem third = new PurchaseItem(3, 3, "third", 1, 1);
-        manager.add(first);
-        manager.add(second);
-        manager.add(third);
-        PurchaseItem[] expected = new PurchaseItem[] {third, second, first};
-        PurchaseItem[] actual = manager.getLast();
+        manager.removeById(idToRemove);
+        PurchaseItem[] expected = new PurchaseItem[]{third, second, first};
+        PurchaseItem[] actual = manager.getAll();
+
         assertArrayEquals(expected, actual);
-    }*/
+        verify(repository).removeById(idToRemove);
+    }
 }
